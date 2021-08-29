80ca776a04aec883d57aea9e791fb6823355e9da
terrotar <pedro_guedes93@hotmail.com>
2021-08-28 23:46:59 -0300

    Started development of bot.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 
 from .blueprints.telegram.routes import telegram
 
+from .config import bot, bot_token, bot_user_name, TOKEN
+
 
 def create_app(config):
     app = Flask(__name__)
--- app/config.py
+++ app/config.py
@@ -1,5 +1,15 @@
 
+import telegram
 
-API_KEY = "1963801639:AAH4De3n2z9r_4AJjL5pI5b_f_qDMplbJ0w"
+
+bot_token = "1963801639:AAH4De3n2z9r_4AJjL5pI5b_f_qDMplbJ0w"
+bot_user_name = "Terrotar_GitWebDevTestBot"
+
+
+TOKEN = bot_token
+bot = telegram.Bot(token=TOKEN)
+
 
 # bot = telebot.TeleBot(API_KEY)
+
+# bot_token, bot_user_name, URL
