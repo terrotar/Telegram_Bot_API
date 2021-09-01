36711b9711f794eb946508770925a85e963b8c3f
terrotar <pedro_guedes93@hotmail.com>
2021-09-01 03:09:50 -0300

    Created table Message with relationship with table User to get messages of certain user_id and archive messages in database.

--- app/models/user.py
+++ app/models/user.py
@@ -10,12 +10,14 @@ class User(db.Model):
     id = db.Column("id_user", db.Integer, primary_key=True)
     __first_name = db.Column("first_name", db.String, nullable=False)
     __last_name = db.Column("last_name", db.String, nullable=False)
-    __cel_number = db.Column("cel_number", db.String, unique=True, nullable=False)
+    __cel_number = db.Column("cel_number", db.BigInteger, unique=True, nullable=False)
+    __messages = db.Column("messages", db.String, unique=True, nullable=False)
 
     def __init__(self, first_name, last_name, cel_number):
         self.__first_name = first_name
         self.__last_name = last_name
         self.__cel_number = cel_number
+        self.__ = []
 
     # GETTERS
     # first_name
