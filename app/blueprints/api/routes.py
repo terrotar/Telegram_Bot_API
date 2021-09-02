8f84b4d878c84e7a6b3dfeed39786db011819724
terrotar <pedro_guedes93@hotmail.com>
2021-09-02 15:24:53 -0300

    Changed Blueprint api route.

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -7,7 +7,7 @@ from ...models.user import User, user_schema, users_schema
 api = Blueprint('api', __name__, url_prefix="/api")
 
 
-@api.route("/", methods=["GET"])
+@api.route("/all_users", methods=["GET"])
 def get_users():
     users = User.query.all()
     all_users = jsonify(users_schema.dump(users))
