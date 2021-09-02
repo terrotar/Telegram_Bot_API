2db46c22979f895de2082b3a2d8e3db47094da59
terrotar <pedro_guedes93@hotmail.com>
2021-09-01 23:47:04 -0300

    Updated some changes inside /models to test database and contact_user.

--- app/models/message.py
+++ app/models/message.py
@@ -6,35 +6,23 @@ from sqlalchemy.ext.hybrid import hybrid_property
 
 class Message(db.Model):
     __tablename__ = "mensagem"
-    id = db.Column("id_mensagem", db.Integer, primary_key=True)
+    __id_message = db.Column("id_mensagem", db.Integer, primary_key=True)
     __text = db.Column("text", db.String, nullable=False)
     __first_name = db.Column("first_name", db.String, db.ForeignKey('usuario.first_name'))
-    __cel_number = db.Column("cel_number", db.BigInteger, db.ForeignKey('usuario.cel_number'))
-    __id_user = db.Column("usid_user", db.Integer, db.ForeignKey('usuario.id_user'))
+    __last_name = db.Column("last_name", db.String, db.ForeignKey('usuario.last_name'))
+    __cel_number = db.Column("cel_number", db.String, db.ForeignKey('usuario.cel_number'))
+    __id_user = db.Column("id_user", db.BigInteger, db.ForeignKey('usuario.id_user'))
 
-    def __init__(self, text, first_name, cel_number, id_user):
+    def __init__(self, text):
         self.__text = text
-        self.__first_name = first_name
-        self.__cel_number = cel_number
-        self.__id_user = id_user
 
     # GETTERS
+    # id_message
+    @hybrid_property
+    def id_message(self):
+        return self.__id_message
+
     # text
     @hybrid_property
     def text(self):
         return self.__text
-
-    # first_name
-    @hybrid_property
-    def first_name(self):
-        return self.first_name
-
-    # cel_number
-    @hybrid_property
-    def cel_number(self):
-        return self.__cel_number
-
-    # id_user
-    @hybrid_property
-    def id_user(self):
-        return self.__id_user
