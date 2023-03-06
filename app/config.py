769d093b88f71bd4a3de58bb652c358d846da2dd
PedroGuedes <pedro_guedes93@hotmail.com>
2023-03-06 14:45:59 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -9,12 +9,12 @@ from flask_marshmallow import Marshmallow
 db = SQLAlchemy()
 
 
-API_KEY = "1963801639:AAH4De3n2z9r_4AJjL5pI5b_f_qDMplbJ0w"
+API_KEY = ""
 
 bot = telegram.Bot(token=API_KEY)
 
 # This key should be private 
-SECRET_KEY = '2\xa8\x1e\x98\x0f:\xce\x95[\xe1\x1fV\x1a\x8drj'
+SECRET_KEY = ''
 
 
 ma = Marshmallow()
